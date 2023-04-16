2cca84aadca94b476b01e23108394f8c2afd57bf
Hakkology <yildizhakan88@gmail.com>
2023-04-16 15:30:50 +0300

    Model-Backend
    DevModels added

---
+++ WebbySoftware/Models/GameDevModel/GameDevelopmentInt.cs
@@ -0,0 +1,7 @@
+﻿namespace WebbySoftware.Models.GameDevModel
+{
+    public interface GameDevelopmentInt
+    {
+        public string ProjectDownloadPath { get; set; }
+    }
+}
---
+++ WebbySoftware/Models/UsedInterfaces/ProjectModule.cs
@@ -0,0 +1,17 @@
+﻿namespace WebbySoftware.Models.Interfaces {
+    public interface ProjectModule {
+
+        //Attributes common to all Project Modules
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public string ProjectVersion { get; set; }
+        public List<string> Thumbnails { get; set; }
+        public string ProjectGitLink { get; set; }
+
+        public void Add();
+        public void Remove();
+        public void Update();
+        public void Delete();
+            
+    }
+}

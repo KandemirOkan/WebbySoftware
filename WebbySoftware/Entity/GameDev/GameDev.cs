e29cbca8199905c15bd8c3018be029f0d3a0c0a3
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2023-06-15 16:02:26 +0300

    Constructors fixed for efcore migration.

--- WebbySoftware/Entity/GameDev/GameDev.cs
+++ WebbySoftware/Entity/GameDev/GameDev.cs
@@ -6,11 +6,13 @@ namespace WebbySoftware.Entity.GameDev
 {
     public class GameDev: IBaseAtrributes, IProjectModule {
 
+        //base attributes
         public DateTime CreationDate { get; set; }
         public DateTime UpdateDate { get; set; }
         public bool Active { get; set; }
         public bool Deleted { get; set; }
 
+        //common attributes
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID {get; set;}
         public string ProjectName { get; set; }
@@ -20,20 +22,12 @@ namespace WebbySoftware.Entity.GameDev
 
         public ICollection<UserGameDev> UserGameDevs { get; set; }
 
-        public GameDev(string _projectName, string _projectDescription, List<string> _thumbnails, string _projectGitLink) {
-            //base attributes
+        public GameDev() {
+
             CreationDate = DateTime.Now;
             UpdateDate = DateTime.Now;
             Active = true;
             Deleted = false;
-
-            //common attributes
-            ProjectName = _projectName;
-            ProjectDescription = _projectDescription;
-            Thumbnails = _thumbnails;
-            ProjectGitLink = _projectGitLink;
-
-            // specific attributes
         }
     }
 }

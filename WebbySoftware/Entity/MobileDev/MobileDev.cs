626a66857e8b7697cf5fa308c9bfc0fd8f8d787c
Hakan Yildiz <107430060+Hakkology@users.noreply.github.com>
2023-06-15 15:50:26 +0300

    Constructors changed for EF Core DB migration

--- WebbySoftware/Entity/MobileDev/MobileDev.cs
+++ WebbySoftware/Entity/MobileDev/MobileDev.cs
@@ -5,38 +5,31 @@ using System.Collections.Generic;
 namespace WebbySoftware.Entity.MobileDev {
     public class MobileDev: IBaseAtrributes, IProjectModule, IMobileDev {
 
+        //base attributes
         public DateTime CreationDate { get; set; }
         public DateTime UpdateDate { get; set; }
         public bool Active { get; set; }
         public bool Deleted { get; set; }
 
+        //common attributes
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID {get; set;}
         public string ProjectName { get; set; }
         public string ProjectDescription { get; set; }
         public List<string> Thumbnails { get; set; }
         public string ProjectGitLink { get; set; }
+
+        // specific attributes
         public string ProjectLink { get; set; }
 
         public ICollection<UserMobileDev> UserMobileDevs { get; set; }
 
-        public MobileDev(string _projectName, string _projectDescription, string _projectVersion, List<string> _thumbnails, string _projectGitLink, string _projectLink, string _projectPath) {
-
-            //base attributes
+        public MobileDev() {
+            
             CreationDate = DateTime.Now;
             UpdateDate = DateTime.Now;
             Active = true;
             Deleted = false;
-
-            //common attributes
-            ProjectName = _projectName;
-            ProjectDescription = _projectDescription;
-            Thumbnails = _thumbnails;
-            ProjectGitLink = _projectGitLink;
-
-            // specific attributes
-            ProjectLink = _projectLink;
-
         }
     }
 }

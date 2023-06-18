7c881db7e603ac9965e9ad9b3e5db1aa7d120205
Hakkology <yildizhakan88@gmail.com>
2023-06-18 22:40:05 +0300

    Database formed using ElephantSQL. Mig success.

---
+++ WebbySoftware/Migrations/20230618191321_webbysoft1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace WebbySoftware.Migrations
+{
+    /// <inheritdoc />
+    public partial class webbysoft1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    ProjectName = table.Column<string>(type: "text", nullable: false),
+                    ProjectDescription = table.Column<string>(type: "text", nullable: false),
+                    Thumbnails = table.Column<List<string>>(type: "text[]", nullable: false),
+                    ProjectGitLink = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MobileApps",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    ProjectName = table.Column<string>(type: "text", nullable: false),
+                    ProjectDescription = table.Column<string>(type: "text", nullable: false),
+                    Thumbnails = table.Column<List<string>>(type: "text[]", nullable: false),
+                    ProjectGitLink = table.Column<string>(type: "text", nullable: false),
+                    ProjectLink = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MobileApps", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Surname = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    RefreshTokenExpireDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    RefreshToken = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WebApps",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CreationDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    UpdateDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Active = table.Column<bool>(type: "boolean", nullable: false),
+                    Deleted = table.Column<bool>(type: "boolean", nullable: false),
+                    ProjectName = table.Column<string>(type: "text", nullable: false),
+                    ProjectDescription = table.Column<string>(type: "text", nullable: false),
+                    Thumbnails = table.Column<List<string>>(type: "text[]", nullable: false),
+                    ProjectGitLink = table.Column<string>(type: "text", nullable: false),
+                    ProjectWebpage = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebApps", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserGameDevs",
+                columns: table => new
+                {
+                    GameID = table.Column<int>(type: "integer", nullable: false),
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserGameDevs", x => new { x.GameID, x.UserID });
+                    table.ForeignKey(
+                        name: "FK_UserGameDevs_Games_GameID",
+                        column: x => x.GameID,
+                        principalTable: "Games",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserGameDevs_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserMobileDevs",
+                columns: table => new
+                {
+                    MobileAppID = table.Column<int>(type: "integer", nullable: false),
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserMobileDevs", x => new { x.MobileAppID, x.UserID });
+                    table.ForeignKey(
+                        name: "FK_UserMobileDevs_MobileApps_MobileAppID",
+                        column: x => x.MobileAppID,
+                        principalTable: "MobileApps",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserMobileDevs_Users_MobileAppID",
+                        column: x => x.MobileAppID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserWebDevs",
+                columns: table => new
+                {
+                    WebAppID = table.Column<int>(type: "integer", nullable: false),
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserWebDevs", x => new { x.UserID, x.WebAppID });
+                    table.ForeignKey(
+                        name: "FK_UserWebDevs_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserWebDevs_WebApps_WebAppID",
+                        column: x => x.WebAppID,
+                        principalTable: "WebApps",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserGameDevs_UserID",
+                table: "UserGameDevs",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserWebDevs_WebAppID",
+                table: "UserWebDevs",
+                column: "WebAppID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserGameDevs");
+
+            migrationBuilder.DropTable(
+                name: "UserMobileDevs");
+
+            migrationBuilder.DropTable(
+                name: "UserWebDevs");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "MobileApps");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "WebApps");
+        }
+    }
+}

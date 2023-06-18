98b3da5f825de78f781747c4453c6538898177aa
Hakkology <yildizhakan88@gmail.com>
2023-06-18 22:54:23 +0300

    Testing appsettings for commit;

--- WebbySoftware/TokenOperations/Token.cs
+++ WebbySoftware/TokenOperations/Token.cs
@@ -2,6 +2,7 @@ namespace WebbySoftware.TokenOperations
 {
     public class Token
     {
+        // token details
         public string AccessToken { get; set; }
         public DateTime Expiration { get; set; }
         public string RefreshToken { get; set; }

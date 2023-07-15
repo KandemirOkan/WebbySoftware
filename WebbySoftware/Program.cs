8a21af8a3f4d6c4164ef917b6ae324e507ac196c
Hakkology <yildizhakan88@gmail.com>
2023-07-15 09:02:09 +0300

    azure error check

--- WebbySoftware/Program.cs
+++ WebbySoftware/Program.cs
@@ -44,12 +44,15 @@ namespace WebbySoftware
 			
 
 			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
+			if (env.IsDevelopment() || env.IsEnvironment("Azure"))
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
 			{
 				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				// The default HSTS value is 30 days.
 				app.UseHsts();
-
 			}
 
 			if (app.Environment.IsDevelopment())
